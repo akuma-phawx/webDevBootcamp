c999b1b0c8b9f09abff6c2e375d0e129a659c4b1
akuma-phawx <chrisvradis00@gmail.com>
2020-10-31 11:58:25 +0200

    Testing

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Hello from our first file");
+let random = Math.random();
+
+if (phrase === "stop") {
+  console.log("red");
+} else if (phrase === "slow") {
+  console.log("yellow");
+} else if (phrase === "go") {
+  console.log("green");
+} else {
+  console.log("purple");
+}
