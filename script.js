8df656f51b05419972b9307ee66be3d123d90b0c
akuma-phawx <chrisvradis00@gmail.com>
2020-11-01 13:18:35 +0200

    Objects

---
+++ script.js
@@ -0,0 +1,24 @@
+const log = console.log.bind(console);
+// Objects
+
+const person = {
+  firstName: "Christopher",
+  lastName: "Vradis",
+  age: 25,
+};
+
+//Accessing Data
+log(person["firstName"]);
+log(person.lastName);
+log(person.age);
+
+const restaurant = {
+  name: "Ichiran Ramen",
+  address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+  city: "Brooklyn",
+  state: "NY",
+  zipcode: "11206",
+};
+
+const fullAddress = `${restaurant.address}, ${restaurant.city}, ${restaurant.state} ${restaurant.zipcode}`;
+log(fullAddress);
