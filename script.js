6035afa62d9854c7a45fbf89c777c9b269adfcc2
akuma-phawx <chrisvradis00@gmail.com>
2020-11-01 15:48:25 +0200

    Loops

--- script.js
+++ script.js
@@ -1,39 +1,21 @@
 const log = console.log.bind(console);
-// Objects
 
-const person = {
-  firstName: "Christopher",
-  lastName: "Vradis",
-  age: 25,
-};
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"];
 
-//Accessing Data
-log(person["firstName"]);
-log(person.lastName);
-log(person.age);
-
-const restaurant = {
-  name: "Ichiran Ramen",
-  address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
-  city: "Brooklyn",
-  state: "NY",
-  zipcode: "11206",
-};
-
-const fullAddress = `${restaurant.address}, ${restaurant.city}, ${restaurant.state} ${restaurant.zipcode}`;
-log(fullAddress);
-
-const comments = [
-  {
-    username: "Tammy",
-    text: "ewwww",
-    votes: 9,
-  },
-  {
-    username: "Fishboy",
-    text: "glub glub glub",
-    votes: 391931,
-  },
+const seatingChart = [
+  ["Kristen", "Erik", "Domna"],
+  ["Christopher", "Pegasus", "Fearwell"],
+  ["Ariel", "Yamaha", "Teddy"],
 ];
 
-log(comments[1].text);
+for (let i = 0; i < seatingChart.length; i++) {
+  for (let j = 0; j < seatingChart[i].length; j++) {
+    log(seatingChart[i][j]);
+  }
+}
+
+let count = 0;
+while (count < 10) {
+  count++;
+  console.log(count);
+}
