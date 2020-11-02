036514eff2d5abf62710f1f631b7efe7e5e0a345
akuma-phawx <chrisvradis00@gmail.com>
2020-11-02 18:24:44 +0200

    Inline Event

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const textToLoop =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
-const container = document.querySelector(".container");
-for (let i = 1; i <= 151; i++) {
-  const pokeContainer = document.createElement("div");
-  const pokeNumber = document.createElement("span");
-  pokeNumber.innerText = `${i}`;
-  const pokemonToAdd = document.createElement("img");
-  const newAnchor = `${i}.png`;
-  pokemonToAdd.src = textToLoop + newAnchor;
-  pokeContainer.append(pokemonToAdd, pokeNumber);
-  pokeContainer.classList.add("toInner");
-  container.appendChild(pokeContainer);
-}
+// const container = document.querySelector(".container");
+// for (let i = 1; i <= 151; i++) {
+//   const pokeContainer = document.createElement("div");
+//   const pokeNumber = document.createElement("span");
+//   pokeNumber.innerText = `${i}`;
+//   const pokemonToAdd = document.createElement("img");
+//   const newAnchor = `${i}.png`;
+//   pokemonToAdd.src = textToLoop + newAnchor;
+//   pokeContainer.append(pokemonToAdd, pokeNumber);
+//   pokeContainer.classList.add("toInner");
+//   container.appendChild(pokeContainer);
+// }
