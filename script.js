417baa3a0555fd7c8b35600488136ddfaf783e1f
akuma-phawx <chrisvradis00@gmail.com>
2020-11-02 20:38:32 +0200

    Event Delegation

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-const form = document.querySelector("#registrationForm");
+const input = document.querySelector("input");
+const h1 = document.querySelector("h1");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const username = form.elements.username;
-  const tweet = form.elements.tweet;
-  addTweet(username.value, tweet.value);
-  username.value = "";
-  tweet.value = "";
+input.addEventListener("input", (e) => {
+  if (input.value.length === 0) {
+    h1.innerText = "Enter your Username";
+  } else {
+    h1.innerText = `Welcome, ${input.value}`;
+  }
 });
-
-const addTweet = (username, tweet) => {
-  const list = document.querySelector("#listTweet");
-  const liToAppend = document.createElement("li");
-  const bold = document.createElement("b");
-  bold.append(username);
-  liToAppend.append(bold);
-  liToAppend.append(` - ${tweet}`);
-  list.appendChild(liToAppend);
-};
