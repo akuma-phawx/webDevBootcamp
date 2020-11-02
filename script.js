e62505b0679b4130ee8cb1e12914b5db1a3b21b3
akuma-phawx <chrisvradis00@gmail.com>
2020-11-02 19:08:22 +0200

    Change button color

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const colorButton = document.querySelector("#colorButton");
-const body = colorButton.parentElement.parentElement;
-colorButton.addEventListener("click", () => {
-  const randomRed = Math.floor(Math.random() * 255) + 1;
-  const randomBlue = Math.floor(Math.random() * 255) + 1;
-  const randomGreen = Math.floor(Math.random() * 255) + 1;
-  body.style.background = `rgb(${randomRed},${randomBlue},${randomGreen})`;
-});
+const allButtons = document.querySelectorAll("button");
+const makeRandColor = () => {
+  const randomRed = Math.floor(Math.random() * 256);
+  const randomBlue = Math.floor(Math.random() * 256);
+  const randomGreen = Math.floor(Math.random() * 256);
+  return `rgb(${randomRed},${randomBlue},${randomGreen})`;
+};
+
+for (const button of allButtons) {
+  button.addEventListener("click", colorize);
+}
+
+function colorize() {
+  this.style.background = makeRandColor();
+}
