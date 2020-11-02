2eeb20d50a5d1183027c77f20618df8ac07c5b24
akuma-phawx <chrisvradis00@gmail.com>
2020-11-02 18:52:59 +0200

    Random Background Color

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-const textToLoop =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
-
-// const container = document.querySelector(".container");
-// for (let i = 1; i <= 151; i++) {
-//   const pokeContainer = document.createElement("div");
-//   const pokeNumber = document.createElement("span");
-//   pokeNumber.innerText = `${i}`;
-//   const pokemonToAdd = document.createElement("img");
-//   const newAnchor = `${i}.png`;
-//   pokemonToAdd.src = textToLoop + newAnchor;
-//   pokeContainer.append(pokemonToAdd, pokeNumber);
-//   pokeContainer.classList.add("toInner");
-//   container.appendChild(pokeContainer);
-// }
-
-const but = document.querySelector("#mybut");
-console.dir(but);
-but.onclick = () => {
-  alert("go away");
-};
+const colorButton = document.querySelector("#colorButton");
+const body = colorButton.parentElement.parentElement;
+colorButton.addEventListener("click", () => {
+  const randomRed = Math.floor(Math.random() * 255) + 1;
+  const randomBlue = Math.floor(Math.random() * 255) + 1;
+  const randomGreen = Math.floor(Math.random() * 255) + 1;
+  body.style.background = `rgb(${randomRed},${randomBlue},${randomGreen})`;
+});
