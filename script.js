b7b4f953535c8d74aa611bdb3618c078a9d38eae
akuma-phawx <chrisvradis00@gmail.com>
2020-11-02 17:33:03 +0200

    Pokemon Sprites

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-const log = console.log.bind(console);
+const textToLoop =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
-const banner = document.getElementById("banner");
-console.dir(banner);
-const toc = document.getElementById("toc");
-console.dir(toc);
-const imgs = document.getElementsByTagName("img");
-console.dir(imgs);
-console.log(imgs[0]);
-
-for (let i = 0; i < imgs.length; i++) {
-  imgs[i].src =
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg";
+const container = document.querySelector(".container");
+for (let i = 1; i <= 151; i++) {
+  const pokeContainer = document.createElement("div");
+  const pokeNumber = document.createElement("span");
+  pokeNumber.innerText = `${i}`;
+  const pokemonToAdd = document.createElement("img");
+  const newAnchor = `${i}.png`;
+  pokemonToAdd.src = textToLoop + newAnchor;
+  pokeContainer.append(pokemonToAdd, pokeNumber);
+  pokeContainer.classList.add("toInner");
+  container.appendChild(pokeContainer);
 }
-
-const sqr = document.getElementsByClassName("square");
-console.dir(sqr);
-
-const h1 = document.querySelector("h1");
-const byid = document.querySelector("#banner");
-const different = document.querySelector("img:nth-of-type(2)");
-log(different);
-
-const doneTodos = document.querySelectorAll(".doneTodos");
-const checkbox = document.querySelector("input[checkbox]");
