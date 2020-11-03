3780b4a87e287c313ddfe4896514b3bf7e841788
akuma-phawx <chrisvradis00@gmail.com>
2020-11-03 19:20:42 +0200

    pROMISES WOOOO

--- script.js
+++ script.js
@@ -23,3 +23,38 @@ const fakeRequestPromise = (url) => {
     }, delay);
   });
 };
+
+const fakeFetchingData = (url) => {
+  return new Promise((resolve, reject) => {
+    const delayTime = Math.floor(Math.random() * 4500) + 500;
+    setTimeout(() => {
+      if (delayTime > 4000) {
+        reject("Connection time out");
+      } else {
+        resolve("Fetched fake data successfully!");
+      }
+    }, delayTime);
+  });
+};
+
+fakeFetchingData("books.com")
+  .then(() => {
+    console.log("it worked 111111111111");
+    fakeFetchingData("books.com/page1")
+      .then(() => {
+        console.log("it worked  2222222222222");
+        fakeFetchingData("books.com/page2")
+          .then(() => {
+            console.log("it worked  3333333333333");
+          })
+          .catch(() => {
+            console.log("Failed 333333333333");
+          });
+      })
+      .catch(() => {
+        console.log("Failed 22222222222");
+      });
+  })
+  .catch(() => {
+    console.log("Failed 111111111111111");
+  });
