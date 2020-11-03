b7d18e091f379773b5b96bd9c386f3aba7dc6f33
akuma-phawx <chrisvradis00@gmail.com>
2020-11-03 18:57:38 +0200

    Callbacks

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-const input = document.querySelector("input");
-const h1 = document.querySelector("h1");
+const body = document.body;
 
-input.addEventListener("input", (e) => {
-  if (input.value.length === 0) {
-    h1.innerText = "Enter your Username";
-  } else {
-    h1.innerText = `Welcome, ${input.value}`;
-  }
+// setTimeout(() => {
+//   body.style.backgroundColor = "orange";
+//   setTimeout(() => {
+//     body.style.backgroundColor = "yellow";
+
+//     setTimeout(() => {
+//       body.style.backgroundColor = "green";
+//       setTimeout(() => {
+//         body.style.backgroundColor = "blue";
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+const delayedColorChange = (newColor, delay, doNext) => {
+  setTimeout(() => {
+    body.style.backgroundColor = newColor;
+    doNext && doNext();
+  }, delay);
+};
+
+delayedColorChange("olive", 1000, () => {
+  delayedColorChange("green", 1000, () => {
+    delayedColorChange("blue", 1000, () => {
+      delayedColorChange("red", 1000, () => {
+        delayedColorChange("orange", 1000, () => {
+          delayedColorChange("magenta", 1000, () => {});
+        });
+      });
+    });
+  });
 });
