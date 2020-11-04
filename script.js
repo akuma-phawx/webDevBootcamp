e38b6dd9d525fbbe6e74ec74e0fc30028baddaa6
akuma-phawx <chrisvradis00@gmail.com>
2020-11-04 09:22:19 +0200

    axios

--- script.js
+++ script.js
@@ -8,11 +8,27 @@
 //   })
 //   .catch((err) => console.log(err));
 
+// const fetchBitcoinPrice = async () => {
+//   try {
+//     const res = await fetch("https://api.cryptonator.com/api/ticker/btc-usd");
+//     const data = await res.json();
+//     console.log(data);
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
 const fetchBitcoinPrice = async () => {
   try {
-    const res = await fetch("https://api.cryptonator.com/api/ticker/btc-usd");
-    const data = await res.json();
+    const res = await axios.get(
+      "https://api.cryptonator.com/api/ticker/btc-usd"
+    );
+    const headers = res.headers;
+    const data = res.data;
+    const req = res.request;
+    console.log(headers);
     console.log(data);
+    console.log(req.status);
   } catch (e) {
     console.log(e);
   }
